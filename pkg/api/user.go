c8d6fd8fbb22934719f0af12a00889cc0c75ad7f
sekthor <sekthor@protonmail.ch>
2023-10-21 20:41:52 +0200

    feat: include venue/artist name in registration

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -9,14 +9,17 @@ import (
 
 func (api *api) Register(c *gin.Context) {
 
-	var user model.User
+	var registerRequest struct {
+		model.User
+		Name string `json:"name"`
+	}
 
-	if c.BindJSON(&user) != nil {
+	if c.BindJSON(&registerRequest) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid data in login form"})
 		return
 	}
 
-	user, err := api.userService.Register(user)
+	user, err := api.userService.Register(registerRequest.User)
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -27,6 +30,7 @@ func (api *api) Register(c *gin.Context) {
 	case 1:
 		artist := model.Artist{}
 		artist.ID = user.ID
+		artist.Name = registerRequest.Name
 		artist.User = user
 		artist, err = api.artistService.Create(artist)
 		if err != nil {
@@ -37,6 +41,7 @@ func (api *api) Register(c *gin.Context) {
 	case 2:
 		venue := model.Venue{}
 		venue.ID = user.ID
+		venue.Name = registerRequest.Name
 		venue.User = user
 		venue, err = api.venueService.Create(venue)
 		if err != nil {
