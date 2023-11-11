f77a256a9cf21ac3ffd6b61682b570c03ce2cd15
sekthor <sekthor@protonmail.ch>
2023-11-11 20:54:34 +0100

    feat(#31): add translation service

--- ui/src/app/components/header/header.component.ts
+++ ui/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -10,16 +11,25 @@ import { UserService } from 'src/app/services/user.service';
 export class HeaderComponent implements OnInit {
 
   isLoggedIn: boolean = false
+  language: string = ""
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(
+    private userService: UserService, 
+    private router: Router, 
+    private translate: TranslateService) { }
 
   ngOnInit(): void {
     this.isLoggedIn = this.userService.isLoggedIn()
+    this.language = this.translate.currentLang
   }
 
   logout() {
     this.userService.logout() 
     this.isLoggedIn = false
     this.router.navigate(["/"]).then(() => window.location.reload())
+  }
+
+  setLanguage(language: string) {
+    this.translate.use(language)
   }
 }
