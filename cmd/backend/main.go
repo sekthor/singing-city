b8f6b06401c85b6993a7af8320921fce9968e057
sekthor <sekthor@protonmail.ch>
2023-11-19 12:51:17 +0100

    fix: add logging to main.go

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -7,14 +7,21 @@ import (
 )
 
 func main() {
+
+	log.Info().Msg("loading configuration")
 	conf := config.LoadConfig()
+
+	log.Info().Msg("creating api")
 	api, err := api.NewApi(conf)
 
 	if err != nil {
 		log.Fatal().Err(err).Msg("could not create api")
 	}
 
+	log.Info().Msg("creating router")
 	router := api.Router()
+
+	log.Info().Msg("starting router running at '" + conf.Server.Host + ":" + conf.Server.Port + "'")
 	err = router.Run(conf.Server.Host + ":" + conf.Server.Port)
 
 	if err != nil {
