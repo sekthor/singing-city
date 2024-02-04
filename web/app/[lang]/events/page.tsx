0402a607558c482322ce9ec135e62a7d445d486b
sekthor <sekthor@protonmail.ch>
2024-02-04 20:49:17 +0100

    feat: add events table

--- web/app/[lang]/events/page.tsx
+++ web/app/[lang]/events/page.tsx
@@ -1,16 +1,17 @@
 "use client"
 
 import Navbar from "@/components/navbar";
-import { Table } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { Event } from "@/model/event";
+import Link from "next/link";
 
 const events: Event[] = [
     { id: "", name: "Songbird Festival 2024", },
 ]
 
-export default function({ params }: { params: { lang: string }}) {
+export default function EventsPage({ params }: { params: { lang: string }}) {
 
     return (
         <>
@@ -26,6 +27,28 @@ export default function({ params }: { params: { lang: string }}) {
                 </TabsList>
 
                 <TabsContent value="upcoming">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Event</TableHead>
+                                <TableHead>Start</TableHead>
+                                <TableHead>End</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                        {
+                            events.map(e => 
+                            
+                            <TableRow key={e.id}>
+                                <TableCell>
+                                    <Link href={`/${params.lang}/events/${e.name}`}>{e.name}</Link>
+                                </TableCell>
+                                <TableCell>{e.start?.toString()}</TableCell>
+                                <TableCell>{e.end?.toString()}</TableCell>
+                            </TableRow>
+                        )}
+                        </TableBody>
+                    </Table>
                 </TabsContent>
 
                 <TabsContent value="past">
