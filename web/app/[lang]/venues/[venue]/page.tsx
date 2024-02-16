84180cfd5ccfc163e8704a48e50a54d01e4a35d7
sekthor <sekthor@protonmail.ch>
2024-02-16 13:41:29 +0100

    feat: venue details page

---
+++ web/app/[lang]/venues/[venue]/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+
+export default function VenueDetailsPage() {
+    return (
+        <>
+        <Navbar lang="en" />
+        <main className="max-w-3xl mx-auto p-2">
+            
+        </main>
+        </>
+    )
+}
